037e99c7f9c5e3a5819ec450ccd60cc8fd7444f4
Кристиян Янков <39673325+ChrisY60@users.noreply.github.com>
2026-03-09 10:58:45 +0100

    Created Spring Boot Backend

---
+++ FitPinServer/src/main/java/org/example/fitpinserver/FitPinServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.fitpinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitPinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitPinServerApplication.class, args);
+    }
+
+}
---
+++ FitPinServer/src/test/java/org/example/fitpinserver/FitPinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fitpinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitPinServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
